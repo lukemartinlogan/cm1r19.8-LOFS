9bc8b202f78af00816812ac912d9f5ccb5514391
lukemartinlogan <lukemartinlogan@gmail.com>
2023-08-10 14:04:09 -0500

    CM1 documentation

--- buildCM1-spack.sh
+++ buildCM1-spack.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 echo "Building CM1-LOFS from spack libraries..."
 echo "Loading module files"
-spack load h5z-zfp
+spack load h5z-zfp%intel
 cd src
 export H5Z_MAIN=`spack find --format "{prefix}" h5z-zfp%intel`
 export ZFP_MAIN=`spack find --format "{prefix}" zfp%intel`
 export H5_MAIN=`spack find --format "{prefix}" hdf5%intel`
 export FORTRAN_MODULES="${H5Z_MAIN}/include"
 export LD_LIBRARY_PATH="${H5Z_MAIN}/lib:${ZFP_MAIN}/lib"
-
 
 if spack find --format "{compiler}" zfp | grep -q intel; then
     echo "It appears ZFP was compiled with Intel. Using Intel Makefile..."
@@ -17,10 +16,6 @@ if spack find --format "{compiler}" zfp | grep -q intel; then
     PREFIX=$PREFIX make -f Makefile.spack.intel newcomm
     PREFIX=$PREFIX make -f Makefile.spack.intel lofs
     PREFIX=$PREFIX make -j 8 -f Makefile.spack.intel all
-elif spack find --format "{compiler}" zfp | grep -q gcc; then
-    export HDF5_FC=mpifort
-    PREFIX=$PREFIX make -f Makefile.spack.gcc clean
-    PREFIX=$PREFIX make -f Makefile.spack.gcc all
 else
-    echo "ZFP wasn't compiled by either gcc or intel, which are currently the only supported configurations. You may have to manually modify the Makefile to work."
+    echo "ZFP wasn't compiled by intel, which is currently the only supported configuration. You may have to manually modify the Makefile to work."
 fi
